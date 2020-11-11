ca7327e5415e4cbbd8367f493012b0902a2c7b72
turusuke <mail@turusuke.com>
2020-11-11 16:22:27 +0900

    feat: mpa 構成に変更

---
+++ src/pages/page_list/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <h2>ページ一覧</h2>
+    <ul>
+      <li>
+        <span>ページ名</span>
+        <span>エントリーポイント</span>
+        <span>テンプレート</span>
+      </li>
+      <li v-for="page in pages" :key="page.entry">
+        <span
+          ><a :href="`/${page.filename}`">{{ page.title }}</a></span
+        >
+        <span>{{ page.entry }}</span>
+        <span>{{ page.template || 'public/index.html' }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import config from '../../../vue.config';
+
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      pages: config.pages
+    };
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 800px;
+  margin: 3em auto;
+}
+
+ul {
+  display: table;
+  border: 1px solid #aaa;
+  border-collapse: collapse;
+}
+
+li {
+  line-height: 2;
+  display: table-row;
+}
+
+li > span {
+  padding: 0.2em;
+  display: table-cell;
+  border-right: 1px solid #aaa;
+  border-bottom: 1px solid #aaa;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+  pages: {
+    index: {
+      // entry for the page
+      title: 'トップページ',
+      entry: 'src/pages/index/main.js'
+    },
+    about: {
+      // entry for the page
+      title: 'About',
+      entry: 'src/pages/about/main.js'
+    },
+    page_list: {
+      // entry for the page
+      title: 'ページ一覧',
+      entry: 'src/pages/page_list/main.js'
+    }
+  }
 };
